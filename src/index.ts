0b075faa957487d059288c20fabe46ccf959f7a6
Michael Van Wickle <mvanwickle@gmail.com>
2020-08-12 18:51:34 -0700

    Remove unused package `canvas-confetti`

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,34 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-import tippyjs from "tippy.js";
+import { html, render } from "lit-html";
+// import tippyjs from "tippy.js";
 
-(function () {
-  console.log("index.ts", tippyjs);
+const dataUrl = new URL("https://holdmypics.com/");
+
+(async function () {
+  const main = document.getElementById("main");
+  if (main !== null) {
+    const data = await getData();
+    if (data !== null) {
+      initMain(main, data);
+    }
+  }
 });
+
+async function getData(): Promise<object | null> {
+  const headers = new Headers({ Accept: "application/json" });
+  try {
+    const res = await fetch(dataUrl.href, { headers });
+    return res.json();
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+function initMain(elem: HTMLElement, data: object) {
+  const template = html``;
+
+  render(template, elem);
+}
